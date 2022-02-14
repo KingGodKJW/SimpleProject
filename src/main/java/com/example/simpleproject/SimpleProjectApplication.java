82f6a4d8d4b272004ba2741bb17153f10443b32a
KingGodKJW <kdy5660@naver.com>
2022-02-14 14:39:34 +0900

    war 배포 설정

--- src/main/java/com/example/simpleproject/SimpleProjectApplication.java
+++ src/main/java/com/example/simpleproject/SimpleProjectApplication.java
@@ -2,9 +2,16 @@ package com.example.simpleproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleProjectApplication {
+public class SimpleProjectApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SimpleProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleProjectApplication.class, args);
